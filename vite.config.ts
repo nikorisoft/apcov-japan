6f723994f2b31a9096641a9295cdbc48c6849b68
nikorisoft <44279059+nikorisoft@users.noreply.github.com>
2024-11-25 22:56:02 +0900

    Fix the public path

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    base: "./",
     resolve: {
         alias: {
             "../../images/backgrounds": "uikit/src/images/backgrounds",
-            "../../images/icons": "uikit/src/images/icons"
-        }
-    }
+            "../../images/icons": "uikit/src/images/icons",
+        },
+    },
 });
